f055f5a33de4686276e4d62bf2a20021f7a5a546
under <stevenfadublagem@gmail.com>
2024-08-06 11:09:33 -0300

    (estudo progamação 2.0)

--- aula5.py
+++ aula5.py
@@ -18,3 +18,46 @@ elif op == 3:
 else:
     print('Opção inválida. Tente novamente!')
 
+# atividade 40
+p1 = int(input('Prmeiro número: '))
+p2 = int(input('Segundo número: '))
+
+if p1 > p2:
+    print('O PRIMEIRO valor é maior')
+elif p1 < p2:
+    print('O SEGUNDO valor é o maior')
+elif p1 == p2:
+    print('Os dois valores são IGUAIS')
+
+# atividade 41
+from datetime import date
+
+anoa = date.today().year
+anon = int(input('Ano de nascimento: '))
+
+idade = (anoa - anon)
+print('Quem nasceu em {} tem {} anos em {}'.format(anon,idade,anoa))
+
+if idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo1 = (18 - idade)
+    print('Você ainda não tem 18 anos. Ainda faltam {} anos para o alistamento'.format(saldo1))
+
+elif idade > 18:
+    saldo2 = (idade - 18)
+    print('Você já deveria ter se alistado a há {} anos'.format(saldo2))
+
+# atividade 42
+p1 = float(input('Primeira nota: '))
+p2 = float(input('Segunda nota: '))
+
+m = (p1 + p2) / 2
+print('Tirando {} e {}, a média do aluno é {}'.format(p1,p2,m))
+
+if 7 > m >= 5:
+    print('O aluno está em RECUPERAÇÃO')
+elif m < 5:
+    print('O aluno está REPROVADO')
+elif m >= 7:
+    print('O aluno está APROVADO.')
